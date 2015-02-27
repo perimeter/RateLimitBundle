5bc87c99893927713e02c2a16da2c8d36323d5e8
Brent Shaffer <bshafs@gmail.com>
2015-02-27 13:51:56 -0700

    fixes container parameters and adds container test

---
+++ Tests/ContainerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Perimeter package.
+ *
+ * (c) Adobe Systems, Inc. <bshafs@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Perimeter\RateLimitBundle\Tests;
+
+use Perimeter\RateLimitBundle\Tests\ContainerLoader;
+use Perimeter\RateLimitBundle\Entity\RateLimitBucket;
+use Perimeter\RateLimitBundle\Throttler\DoctrineThrottler;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+class ContainerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testContainerServices()
+    {
+        // build container from services.xml
+        $container = ContainerLoader::buildTestContainer();
+
+        // mock parameters that should be there from symfony but arent
+        $container->setParameter('kernel.debug', true);
+        $container->set('templating', $this->getMock('Symfony\Bundle\FrameworkBundle\Templating\EngineInterface'));
+        $container->set('security.context', $this->getMock('Symfony\Component\Security\Core\SecurityContextInterface'));
+
+        $container->get('perimeter.rate_limit.throttler');
+        $container->get('perimeter.rate_limit.redis_client');
+        $container->get('perimeter.rate_limit.throttler.redis');
+        $container->get('perimeter.rate_limit.throttler.doctrine');
+        $container->get('perimeter.rate_limit.meter_api_controller');
+        $container->get('perimeter.rate_limit.over_limit_controller');
+        $container->get('perimeter.rate_limit.storage');
+        $container->get('perimeter.rate_limit.storage.doctrine');
+        $container->get('perimeter.rate_limit.storage.memory');
+        $container->get('perimeter.rate_limit.storage.admin');
+        $container->get('perimeter.rate_limit.meter_resolver');
+        $container->get('perimeter.rate_limit.listener');
+    }
+}
